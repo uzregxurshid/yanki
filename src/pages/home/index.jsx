1e7e594337c3763bf39a0407737b62a3298c81e7
uzregxurshid <uzregxurshid@gmail.com>
2022-05-23 22:53:54 +0500

    navbar fixed

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,6 +1,10 @@
 import Logo from '../../assets/images/header/YANKI.svg';
 import Favorites from '../../assets/images/header/favorit.svg';
 import Cart from '../../assets/images/header/cart.svg';
+import Profile from '../../assets/images/header/profile.svg';
+import Search from '../../assets/images/header/search.svg';
+import { Link } from 'react-router-dom';
+import './style.css';
 const Home = () => {
   const style = {
     header: 'header',
@@ -13,16 +17,25 @@ const Home = () => {
     navbarHamburger2: 'bg-white block w-[26px] h-1 mt-1.5',
     navbarHamburger3: 'bg-white block w-[18px] h-1 mt-1.5',
     selectLanguage:
-      'w-full h-[19px] ml-[22px] cursor-pointer bg-transparent font-Raleway font-light border-none text-base leading-[19px] text-white',
-    logo: 'basis-38 ml-[-15px]',
+      'flex md:hidden w-full h-[19px] ml-[22px] cursor-pointer bg-transparent font-Raleway font-light border-none text-base leading-[19px] text-white',
+    logo: 'basis-38 ml-[-20px] md:ml-1',
     left: 'flex items-center',
-    cart: 'ml-5',
-    right: ''
+    cart: 'ml-5 hover:animate-pulse active:animate-ping',
+    right: 'flex',
+    favorit: 'hover:animate-pulse active:animate-ping md:ml-5',
+    profile: 'hidden md:inline hover:animate-pulse active:animate-ping md:ml-5',
+    search: 'hidden md:inline hover:animate-pulse active:animate-ping',
+    list: 'hidden md:flex ml-[30px] lg:ml-[104px]',
+    listItem: 'ml-3 lg:ml-6',
+    listLink: 'text-base text-white font-Raleway font-light hover:animate-pulse',
+    selectLang:
+      'hidden md:flex w-full h-[19px] ml-[22px] cursor-pointer bg-transparent font-Raleway font-light border-none text-base leading-[19px] text-white',
+    selectMoney:
+      'hidden md:flex w-full h-[19px] ml-[22px] cursor-pointer bg-transparent font-Raleway font-light border-none text-base leading-[19px] text-white',
+    langmon: 'flex mr-5 lg:mr-[75px]'
   };
   return (
-    <div
-      className={style.header}
-      style={{ backgroundImage: 'url(/assets/header/yankibg.png)', backgroundSize: 'cover' }}>
+    <div className={style.header}>
       <div className={style.container}>
         <div className={style.headerContainer}>
           <div className={style.navbar}>
@@ -43,6 +56,23 @@ const Home = () => {
                   EN
                 </option>
               </select>
+              <ul className={style.list}>
+                <li className={style.listItem}>
+                  <Link className={style.listLink} to={'/catalog'}>
+                    NEW
+                  </Link>
+                </li>
+                <li className={style.listItem}>
+                  <Link className={style.listLink} to={'/catalog'}>
+                    КАТАЛОГ
+                  </Link>
+                </li>
+                <li className={style.listItem}>
+                  <Link className={style.listLink} to={'/catalog'}>
+                    О НАС
+                  </Link>
+                </li>
+              </ul>
             </div>
             <div className={style.logo}>
               <a className={style.logoLink} href="/">
@@ -50,7 +80,37 @@ const Home = () => {
               </a>
             </div>
             <div className={style.right}>
+              <div className={style.langmon}>
+                <select className={style.selectLang}>
+                  <option className={style.option} value="ru">
+                    RU
+                  </option>
+                  <option className={style.option} value="ua">
+                    UA
+                  </option>
+                  <option className={style.option} value="en">
+                    EN
+                  </option>
+                </select>
+                <select className={style.selectMoney}>
+                  <option className={style.option} value="uah">
+                    UAH
+                  </option>
+                  <option className={style.option} value="usd">
+                    USD
+                  </option>
+                  <option className={style.option} value="ru">
+                    RUB
+                  </option>
+                </select>
+              </div>
               <div className={style.buttons}>
+                <button className={style.search}>
+                  <img src={Search} width={20} height={20} />
+                </button>
+                <button className={style.profile}>
+                  <img src={Profile} width={20} height={20} />
+                </button>
                 <button className={style.favorit} type="button">
                   <img src={Favorites} width={20} height={20} />
                 </button>
