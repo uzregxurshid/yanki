95efc43d17c8ce8783c19b895106ccf129531a58
uzregxurshid <uzregxurshid@gmail.com>
2022-06-01 21:17:34 +0500

    fixes

--- src/components/catalog/body/index.jsx
+++ src/components/catalog/body/index.jsx
@@ -1,7 +1,8 @@
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
+import AccordionImg from '../../../assets/images/category/accordion.svg';
 const Category = () => {
   useEffect(() => {
     AOS.init({
@@ -9,6 +10,7 @@ const Category = () => {
     });
     AOS.refresh();
   }, []);
+  const catAccordion = useRef();
   const style = {
     cat: '',
     container: 'max-w-[1160px] w-full mx-auto  px-5 block',
@@ -20,7 +22,12 @@ const Category = () => {
     itemnoFirst:
       'marker:content-rightRow  marker:left-[20px] marker:absolute  marker:z-20 ml-1.5 relative list-inside',
     itemLinknoFirst:
-      'font-Raleway ml-1.5 not-italic font-light text-[16px] leading-[19px] color-[#252525] transition duration-500 hover:text-[#E0BEA2]'
+      'font-Raleway ml-1.5 not-italic font-light text-[16px] leading-[19px] color-[#252525] transition duration-500 hover:text-[#E0BEA2]',
+    accordionButton:
+      'flex w-full h-10 mt-[15px] items-center justify-center bg-[#E0BEA2]  cursor-pointer transition duration-500 hover:drop-shadow-xl active:bg-[#CCA88A]  md:hidden',
+    accordionSpan:
+      'font-Raleway not-italic font-light text-white text-[18px] leading-[21px] mr-[9px]',
+    accordionImg: 'accordion-img transition duration-500'
   };
 
   return (
@@ -41,7 +48,57 @@ const Category = () => {
               </li>
             </ul>
           </div>
-          <div className="catalog">Каталог</div>
+          <button
+            className={style.accordionButton}
+            type="button"
+            onClick={(e) => {
+              catAccordion.current.classList.toggle('max-h-0');
+              catAccordion.current.classList.toggle('max-h-[200px]');
+              document.querySelector('.accordion-img').classList.toggle('rotate-180');
+              console.log(e.target.childNodes);
+            }}>
+            <span className={style.accordionSpan}>Каталог</span>
+            <img className={style.accordionImg} src={AccordionImg} alt="accordion" />
+          </button>
+          <ul
+            ref={catAccordion}
+            className="w-full flex flex-col items-center max-h-0 transition-all duration-500  overflow-hidden">
+            <li className="py-2.5 flex justify-center items-center border-b-[.25px] border-black w-full">
+              <Link
+                className="font-Raleway font-light text-[#252525]  hover:animate-pulse hover:text-[#E0BEA2] transition duration-500 text-[16px] leading-[19px]"
+                to={'/catalog/new'}>
+                New
+              </Link>
+            </li>
+            <li className="py-2.5 flex justify-center items-center border-b-[.25px] border-black w-full">
+              <Link
+                className="font-Raleway font-light text-[#252525]  hover:animate-pulse hover:text-[#E0BEA2] transition duration-500 text-[16px] leading-[19px]"
+                to={'/catalog/bestsellers'}>
+                Bestsellers
+              </Link>
+            </li>
+            <li className="py-2.5 flex justify-center items-center border-b-[.25px] border-black w-full">
+              <Link
+                className="font-Raleway font-light text-[#252525]  hover:animate-pulse hover:text-[#E0BEA2] transition duration-500 text-[16px] leading-[19px]"
+                to={'/catalog/furcoats'}>
+                Шубы
+              </Link>
+            </li>
+            <li className="py-2.5 flex justify-center items-center border-b-[.25px] border-black w-full">
+              <Link
+                className="font-Raleway font-light text-[#252525]  hover:animate-pulse hover:text-[#E0BEA2] transition duration-500 text-[16px] leading-[19px]"
+                to={'/catalog/coats'}>
+                Пальто
+              </Link>
+            </li>
+            <li className="py-2.5 flex justify-center items-center border-b-[.25px] border-black w-full">
+              <Link
+                className="font-Raleway font-light text-[#252525]  hover:animate-pulse hover:text-[#E0BEA2] transition duration-500 text-[16px] leading-[19px]"
+                to={'/catalog/showall'}>
+                Посмотреть всё
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
